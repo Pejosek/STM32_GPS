e458e29b5e868a529b486f4d821f4ce51e233b2c
Przemysław Cichocki <pejosek@gmail.com>
2018-07-18 21:06:21 +0200

    first commit

---
+++ SW4STM32/Projekt_GPS_2xUART/Debug/Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ex.d
@@ -0,0 +1,80 @@
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_flash_ex.o: \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Inc/stm32f4xx_hal_conf.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Inc/main.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Inc/stm32f4xx_hal_conf.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Inc/main.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Projekt_GPS_2xUART/Projekt_GPS_2xUART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
